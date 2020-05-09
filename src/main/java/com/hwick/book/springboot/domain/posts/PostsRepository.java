6af9014df0b93f8ee87c13327a5ec0f0fb6b76f9
Injae <hwick@nate.com>
2020-05-09 21:41:49 +0900

    chapter4 mustache로 화면 구성 commit

--- src/main/java/com/hwick/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/hwick/book/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,13 @@
 package com.hwick.book.springboot.domain.posts;
 
-import javafx.geometry.Pos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // DAO = Repository, JpaRepository <Entity 클래스, PK 타입>을 상속하면 기본적인 CRUD 메소드가 자동 생성 됨
 public interface PostsRepository extends JpaRepository <Posts, Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
